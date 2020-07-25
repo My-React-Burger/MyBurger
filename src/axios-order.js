fd4a650c78040a667c3ee6f3184da70261b0830e
MohammadFazil786 <59966554+MohammadFazil786@users.noreply.github.com>
2020-07-25 23:43:03 +0530

    Update axios-order.js

--- src/axios-order.js
+++ src/axios-order.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
 const instances=Axios.create({
-    baseURL:'https://react-my-burger-a7764.firebaseio.com/'
+    baseURL:'ThisUrlwillbeaddedbyyou'
 });
 export default instances;
